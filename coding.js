e31cdd6bb295cc77f712049578895002513c692f
SiriKalidindi <94727938+SiriKalidindi@users.noreply.github.com>
2021-11-22 20:44:53 -0800

    Add files via upload

---
+++ coding.js
@@ -0,0 +1,55 @@
+var playerPaddle= createSprite(390,200,10,100);
+    playerPaddle.shapeColor="blue";
+    var computerPaddle= createSprite(10,200,10,100);
+    computerPaddle.shapeColor="red";
+    var ball= createSprite(200,200,10,10);
+ball.velocityX = 2;
+ball.velocityY = 3;
+    ball.shapeColor="yellow";
+
+function draw() {
+  background("white");
+  if(ball.isTouching(playerPaddle) || ball.isTouching(computerPaddle))
+  {
+    playSound("hit.mp3");
+  }
+  
+  
+  if (keyDown("up")) {
+    playerPaddle.y=playerPaddle.y-10;
+  }
+  
+  if (keyDown("down")) {
+    playerPaddle.y=playerPaddle.y+10;
+  }
+  
+  if(keyDown("space"))
+  {
+     ball.velocityX=2;
+     ball.velocityY=3;
+  }
+  
+  computerPaddle.y=ball.y;
+
+  drawnet();
+  
+ 
+    
+  createEdgeSprites();
+  ball.bounceOff(topEdge);
+  ball.bounceOff(bottomEdge);
+  ball.bounceOff(computerPaddle);
+  ball.bounceOff(playerPaddle);
+  drawSprites();
+  
+}
+
+
+
+function drawnet()
+{  
+  for(var num=0;num<400;num=num+20)
+  {
+    line(200,num,200,num+10);
+  }
+}
